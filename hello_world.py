e403537b2fbbf3d97f9d8157f9769c0e3091b1f3
Noah-Sumayasa <n_sumayasa2024@coloradocollege.edu>
2025-10-28 11:59:05 -0600

    Importing Ev228 data

---
+++ hello_world.py
@@ -0,0 +1,1 @@
+print('Sphink of the black quartz, judge my vow')
---
+++ import_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+path_data = 'C:\Users\Ev228\Downloads\EV228 Data'
+fn_data = 'KRDU_temp_188708-202508.csv'
+
+df_cos = pd.read_csv(fn_data)
