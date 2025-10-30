9db15d1ebc9627e8808b969f0d15039cb8da9262
Noah-Sumayasa <n_sumayasa2024@coloradocollege.edu>
2025-10-30 00:13:44 -0600

    Added begging of code for graphing

--- fnc_process_data.py
+++ fnc_process_data.py
@@ -1,8 +1,30 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 def process_data(file, variable1, variable2):
     df_cos = pd.read_csv(file)
-    
+    ''' Imports data file and returns the data parsed from the column specified
+
+    Arguments:
+    file -- the path name that directs to the data set desired 
+    variable1 -- when set to a specific string, parses for that in the file
+    variable2 -- when set to a specific string, parses for that in the file
+
+    Outputs:
+    var1 -- the data parsed indicated at the string in variable1
+    var2 -- the data parsed indicatated at the string in variable2
+    '''
     var1 = df_cos[variable1]
     var2 = df_cos[variable2]
-    
+    return var1, var2
 
+def plot_data(Output):
+    fig = plt.figure()
+
+    plt.plot(Output, color='red', label=str(Variable1))
+    plt.xlabel(str(variable1))
+    plt.ylabel('temperature, deg F')
+    plt.legend()
+    plt.show()
+    
--- function_test.py
+++ function_test.py
@@ -1,4 +1,9 @@
 import fnc_process_data as fncp
-Output = fncp.process_data('C:\\Users\\Ev228\\Downloads\\EV228_Data\\AYW00090001_temp_195702-202508.csv','YEAR','metANN')
+Output = fncp.process_data('C:\ \Users\\Ev228\\Downloads\\EV228_Data\\AYW00090001_temp_195702-202508.csv','YEAR','metANN')
+''' 
+imports the function indicated by the file name "fnc_process_data" as "fncp" and runs it with the chosen data
 
+Arguments:
+
+'''
 print(Output)
