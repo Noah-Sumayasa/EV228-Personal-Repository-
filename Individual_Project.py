1d294b5fe026a01091f697490ab0a7d61eb02b4e
Noah-Sumayasa <n_sumayasa2024@coloradocollege.edu>
2025-11-10 00:10:33 -0700

    Changes to ind. project code

--- Individual_Project.py
+++ Individual_Project.py
@@ -2,15 +2,24 @@ import matplotlib.pyplot as plt
 import fnc_process_data as fncp
 from mpl_toolkits.basemap import Basemap, shiftgrid
 import numpy as np
+#import cartopy.crs as ccrs
 
 latsnow, longsnow, Snowvar, snowvalt = fncp.process_grid_data('C:\\Users\\Ev228\\Downloads\\EV228_Data\\Da_1.nc','latitude', 'longitude', 'lsf', 'valid_time')
 lattemp, longtemp, Tempvar, tempvalt = fncp.process_grid_data('C:\\Users\\Ev228\\Downloads\\EV228_Data\\Da_2.nc','latitude', 'longitude', 't2m', 'valid_time')
 
 Snowvarmean = Snowvar.mean('valid_time')
+
+
+'''Baseline approach - which isn't working'''
 #print(grid_data)
 
 #fncp.Plot_Grid_Data(grid_data['mean'], 'title', 'C:\\Users\\Ev228\\Downloads\\EV228_Data')
-topoin,lons = shiftgrid(-180.,Snowvar,longsnow,start=False)
+#Wraplon = (longsnow + 180) % 360 - 180
+#longsnow2 = Wraplon.sortby(longsnow)
+#Wraplat = (latsnow + 180) % 360 -180
+#latsnow2 = Wraplat.sortby(latsnow)
+lonsin = 66, 125
+longsnow2 = shiftgrid(-180., longsnow, lonsin, start=False)
 
 m = Basemap(projection='mill',llcrnrlat=29-1,urcrnrlat=54 +1,\
             llcrnrlon=-122 -1,urcrnrlon=-66 +1,resolution='l')
@@ -23,7 +32,7 @@ m.drawmapboundary(fill_color="#383838")
 #m.drawparallels(parallels, labels=[True, False, False, False], fontsize=10, linewidth=0.001)
 #meridians = np.arange(-180., 181., 5.) # Draw meridians every 60 degrees
 #m.drawmeridians(meridians, labels=[False, False, False, True], fontsize=10, linewidth=0.001)
-m.pcolor(longsnow,latsnow,np.squeeze(Snowvarmean))
+m.pcolor(longsnow2, latsnow,np.squeeze(Snowvarmean))
 plt.colorbar(label = 'snowfall')
 plt.title("Mean extreme Snowfall events in North America")
 plt.show()
